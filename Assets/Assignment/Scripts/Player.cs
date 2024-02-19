569b60ab322b81aa5f17b7e1f9f93dd91aa5a85d
DESKTOP-B7B83OD\James <jmunozsevillano@gmail.com>
2024-02-19 17:29:36 -0500

    Feature 2: Character that moves with mouse input and changes animations depending on factors

---
+++ Assets/Assignment/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 5f;
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movement = destination - (Vector2)transform.position;
+        if (movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        animator.SetFloat("Movement", movement.magnitude);
+        animator.SetFloat("Direction", movement.x);
+    }
+}
